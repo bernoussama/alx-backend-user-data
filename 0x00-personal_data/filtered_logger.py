ba6e238c69f2f1d36f180a45155c7fc6be867c60
bernoussama <oussama.bernou@outlook.com>
2024-11-07 21:51:31 +0100

    remove type hints

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,12 +4,9 @@
 """
 
 import re
-from typing import List
 
 
-def filter_datum(
-    fields: List[str], redaction: str, message: str, separator: str
-):
+def filter_datum(fields, redaction, message, separator):
     """Filters a message for personal data"""
     for field in fields:
         message = re.sub(r"(?<={}=)[^;]*".format(field), redaction, message)
