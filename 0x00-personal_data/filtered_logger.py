210da60dd39de4205f43e213b30e4b23efaca51b
bernoussama <oussama.bernou@outlook.com>
2024-11-07 22:21:53 +0100

    task 4

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -69,3 +69,24 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         database=database,
     )
     return connection
+
+
+def main():
+    """read and filter data"""
+    fields = "name, email, phone, ssn, password, ip, last_login, user_agent"
+    columns = fields.split(", ")
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda a: "{}={}".format(a[0], a[1]),
+                zip(columns, row),
+            )
+            msg = "{};".format("; ".join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
