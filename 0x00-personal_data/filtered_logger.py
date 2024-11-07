a0fb2fb79843c6bed5f2fff252d2affc3441fd19
bernoussama <oussama.bernou@outlook.com>
2024-11-07 21:57:02 +0100

    rewrite

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,10 +4,17 @@
 """
 
 import re
+from typing import List
+
+patterns = {
+    "extract": lambda x, y: r"(?P<field>{})=[^{}]*".format("|".join(x), y),
+    "replace": lambda x: r"\g<field>={}".format(x),
+}
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """Filters a message for personal data"""
-    for field in fields:
-        message = re.sub(r"(?<={}=)[^;]*".format(field), redaction, message)
-    return message
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
