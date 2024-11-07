17bade04469697827b49f4c86eda38f3b3e05217
bernoussama <oussama.bernou@outlook.com>
2024-11-07 22:30:00 +0100

    check if password valid

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Encrypts a password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if a password is valid"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
