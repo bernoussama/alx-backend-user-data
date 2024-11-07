ff8c325950be45c49078a288ef58ae81c8837905
bernoussama <oussama.bernou@outlook.com>
2024-11-07 21:50:04 +0100

    function under 5 lines

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,9 +10,7 @@ from typing import List
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ):
-    """
-    Filters a message for personal data
-    """
+    """Filters a message for personal data"""
     for field in fields:
         message = re.sub(r"(?<={}=)[^;]*".format(field), redaction, message)
     return message
