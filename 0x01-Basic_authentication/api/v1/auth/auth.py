450c5debc8e45bf77a47a17ab657e5a92d654f8f
bernoussama <oussama.bernou@outlook.com>
2024-11-13 00:16:36 +0100

    feat: request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,6 +23,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
+        if request is not None:
+            if request.headers.get("Authorization") is not None:
+                return request.headers.get("Authorization")
         return None
 
     def current_user(self, request=None) -> TypeVar("User"):
