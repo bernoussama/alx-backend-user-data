a39e46a4ebc9304bd4d7509cf8940c9bf4c76aa3
bernoussama <oussama.bernou@outlook.com>
2024-11-12 23:54:05 +0100

    feat: require_auth func

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from typing import List, TypeVar
 from flask import request
+import re
 
 
 class Auth:
@@ -10,7 +11,15 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require authentication"""
-        return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                if exclusion_path.endswith("*"):
+                    pattern = re.escape(exclusion_path[:-1]) + ".*"
+                else:
+                    pattern = re.escape(exclusion_path.rstrip("/")) + "/?"
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
