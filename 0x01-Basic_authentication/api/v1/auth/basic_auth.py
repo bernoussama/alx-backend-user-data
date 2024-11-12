afc34f4ea50edb41cb7267998a42e32d0e51e53f
bernoussama <oussama.bernou@outlook.com>
2024-11-13 00:38:43 +0100

    feat: decode base64 auth header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,18 +4,33 @@
 
 from .auth import Auth
 import re
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
     """Basic authentication class."""
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """Extracts the Base64 part of the Authorization header
-        for a Basic Authentication.
-        """
+        """Extracts the Base64 part of the Authorization header"""
         if type(authorization_header) is str:
             pattern = r"Basic (?P<token>.+)"
             field_match = re.fullmatch(pattern, authorization_header.strip())
             if field_match is not None:
                 return field_match.group("token")
         return None
+
+    def decode_base64_authorization_header(
+        self,
+        base64_authorization_header: str,
+    ) -> str:
+        """Decodes base64-encoded authorization header"""
+        if type(base64_authorization_header) is str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode("utf-8")
+            except (binascii.Error, UnicodeDecodeError):
+                return None
