0610ece9a1fd319af17a3f21c48eed379351f4ed
bernoussama <oussama.bernou@outlook.com>
2024-11-12 22:48:16 +0100

    fixx

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 """ Auth module
 """
-
 from flask import request
 
 
 class Auth:
-    """Auth class
-    """
+    """Auth class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Require authentication""""
+        """Require authentication"""
         return False
+
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
         return None
-    def current_user(self, request=None) -> TypeVar('User'):
+
+    def current_user(self, request=None) -> TypeVar("User"):
         """Current user"""
         return None
